05711f54ab6a5ed83b80dc008d444fe88b4f7a9e
muhamad afat <>
2024-12-03 23:07:28 +0700

    Backend: Menambah models dan migrations

---
+++ app/Models/Address.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'first_name',
+        'last_name',
+        'phone',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+    ];
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getFullNameAttribute(){
+        return "{this->first_name} {this->last_name}";
+    }
+}
