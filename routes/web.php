525e2dd894b5a0cc756ce10df0fb4d7f1682510f
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-03 16:51:46 +0700

    membuat tampilan homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
