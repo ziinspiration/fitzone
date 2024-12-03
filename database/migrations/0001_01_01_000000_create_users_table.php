0e4e63de702375436e461554b4bf8147de727d28
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-03 23:50:50 +0700

    revisi tampilan homepage

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('utype')->default('USR')->comment('ADM for Admin and User or Customer');
             $table->rememberToken();
             $table->timestamps();
         });
