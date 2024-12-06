daac27ba314751fe848754f551b47cc2032738fc
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-06 17:51:04 +0700

    Frontend-1: Mengerjakan halaman dashboard

---
+++ app/Models/Product.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\OrderItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale'
+    ];
+
+    protected $casts = [
+        'images' => 'array', 
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
