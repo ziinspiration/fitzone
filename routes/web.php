4519b369ee11846d2191ae453bd349356832683b
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-10 02:03:51 +0700

    Frontend-1 : Menambahkan CategoryPages dan ProductPages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Livewire\HomePage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
