824dd5b7eeca3a99dc63fcd064fcd159621efdc3
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-09 23:31:58 +0700

    Frontend-1 : Mengerjalan HomePage UI (testing livewire)

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h2 class="text-4xl text-red-500">HELLO WORLD TESTING LIVEWARE</h2>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "node_modules/preline/dist/*.js",
     ],
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
-    plugins: [],
+    plugins: [require("preline/plugin")],
 };
