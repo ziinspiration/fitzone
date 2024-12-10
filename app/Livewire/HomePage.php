63a07904787ccf6e91096b8a5531bc6036324033
DaffaRiyadi03 <daffariyadi100@gmail.com>
2024-12-10 23:56:46 +0700

    Frontend-2 : Merubah title halaman

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Home Page - Fitzone')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+
+        return view('livewire.home-page', [
+            'brands' => $brands
+        ]);
     }
 }
