d5b1150e451d5cb99f5a1d410f4a3cc94053c841
muhamad afat <>
2024-12-11 01:02:32 +0700

    Backend-1 : Mengerjakan Controller ProductPages

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Product - DCodeMania')]
 class ProductsPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1); 
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6), 
+        ]);
     }
 }
