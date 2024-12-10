ead7d0ffdc8e04a59356a032538744e2afae9820
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-11 02:22:27 +0700

    Frontend-1 : Mengerjakan tampilan ProductDetail

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+
+#[Title('Product Detail - FitZone')]
 class ProductDetailPage extends Component
 {
+
+    public $slug;
+
+    public function mount($slug) {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
