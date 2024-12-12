5ea0f0e9a3d88da1aaa159cd9ccfcdb0c54cad7c
dzikrisee <marlianawatiazzahra@gmail.com>
2024-12-12 21:58:17 +0700

    Revisi komponen Card Produk

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -90,6 +90,9 @@
                             </div>
                         </div>
                     </div>
+
+
+                    
                     <div class="flex flex-wrap items-center ">
 
                         @foreach ($products as $product)
@@ -97,7 +100,7 @@
                                 <div class="border border-gray-300 dark:border-gray-700">
                                     <div class="relative bg-gray-200">
                                         <a href="/products/{{ $product->slug }}" class="">
-                                                <img src="{{ url('storage', $product->images[0]) }}"
+                                                <img src="{{ url('storage', $product->images) }}"
                                                     alt="{{ $product->name }}"
                                                     class="object-cover w-full h-56 mx-auto">
                                         </a>
@@ -115,7 +118,7 @@
                                         </p>
                                     </div>
                                     <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
-                                        <a href="#"
+                                        <a wire:click.prevent='addToCart({{ $product->id }})' href="#"
                                             class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                                 fill="currentColor" class="w-4 h-4 bi bi-cart3" viewBox="0 0 16 16">
@@ -129,8 +132,49 @@
                             </div>
                         @endforeach
 
-
                     </div>
+
+
+                    {{-- Card Produk versi 2 --}}
+                    {{-- <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+                        @foreach ($products as $product)
+                        <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#" class="block">
+                                <img class="p-8 rounded-t-lg w-full h-64 object-cover" src="{{ url('storage', $product->images) }}" alt="{{ $product->name }}" />
+                            </a>
+                            <div class="px-5 pb-5">
+                                <a href="#">
+                                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                                        {{ $product->name }}
+                                    </h5>
+                                </a>
+                                <div class="flex items-center mt-2.5 mb-5">
+                                    <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                                        @for ($i = 0; $i < 4; $i++)
+                                            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                                            </svg>
+                                        @endfor
+                                        <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                                        </svg>
+                                    </div>
+                                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp. {{ $product->price }}</span>
+                                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div> --}}
+                    
+
+
+
                     <!-- pagination start -->
                     <div class="flex justify-end mt-6">
                         {{ $products->links() }}
