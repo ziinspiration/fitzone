8c13e6fac8167ef7e7d69ed065bcd8ddb54c8ac8
ziinspiration <ramddbgk@gmail.com>
2025-01-02 01:32:02 +0700

    Backend : Memperbarui Fungsi Orders

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::middleware('auth')->group(function () {
 
 
     Route::get('/checkout', CheckoutPage::class)->name('checkout.page');
-    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders', MyOrdersPage::class)->name('my-orders');
     Route::get('/my-orders/{order_id}', MyOrderDetailPage::class)->name('my-orders.show');
     Route::get('/success', SuccessPage::class)->name('success');
     Route::get('/cancel', CancelPage::class)->name('cancel');
