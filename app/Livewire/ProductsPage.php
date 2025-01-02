d03c3644fdf7a4b5f55239863c31d0c3ca97c2bb
ziinspiration <ramddbgk@gmail.com>
2025-01-02 22:55:39 +0700

    Membuat fitur Livesearching ( Products )

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -37,16 +37,23 @@ class ProductsPage extends Component
     {
         $total_count = CartManagement::addItemToCart($product_id, 1);
         $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
-        $this->alert('success', 'Success added to cart successfuly!', [
+        $this->alert('success', 'Successfully added to cart!', [ // Memperbaiki typo di sini
             'position' => 'bottom-end',
             'timer' => 3000,
-            'toast' => 'true',
+            'toast' => true, // Mengubah 'true' menjadi boolean
         ]);
     }
 
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
+
+        if ($this->search) {
+            $productQuery->where(function ($query) {
+                $query->where('name', 'like', '%' . $this->search . '%')
+                    ->orWhere('description', 'like', '%' . $this->search . '%');
+            });
+        }
 
         if (!empty($this->selected_categories)) {
             $productQuery->whereIn('category_id', $this->selected_categories);
@@ -63,6 +70,11 @@ class ProductsPage extends Component
         if ($this->on_sale) {
             $productQuery->where('on_sale', 1);
         }
+
+        $productQuery->when($this->sort === 'latest', fn($q) => $q->latest())
+            ->when($this->sort === 'price_low', fn($q) => $q->orderBy('price'))
+            ->when($this->sort === 'price_high', fn($q) => $q->orderByDesc('price'))
+            ->when($this->sort === 'name', fn($q) => $q->orderBy('name'));
 
         return view('livewire.products-page', [
             'products' => $productQuery->paginate(9),
